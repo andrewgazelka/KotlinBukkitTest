2f79f380732b818997868b50af2405c68870279d
Andrew Gazelka <andrew.gazelka@gmail.com>
2018-04-10 17:04:30 -0500

    make code less DRY

--- src/main/kotlin/com/github/andrewgazelka/kotlintest/Config.kt
+++ src/main/kotlin/com/github/andrewgazelka/kotlintest/Config.kt
@@ -1,17 +1,35 @@
 package com.github.andrewgazelka.kotlintest
 
+import com.github.andrewgazelka.kotlintest.Config.cm
 import java.io.File
 
 object Config {
 
     private val file = File("config.yml")
-    private val cm = ConfigManager(file)
+    val cm = ConfigManager(file)
 
-    var period_tick = 1 //TODO: this is quite bad... a default value shouldn't need to be set
+    var period_tick = 1
+    var start_message = "Hello"
+
 
     fun reload() {
         cm.load()
-        period_tick = cm.getDNESet("kotlintest.timer.period_ticks", 1).result
+        period_tick = period_tick.gds("kotlin_test.timer.period_ticks")
+        start_message = start_message.gds("kotlin_test.message.start")
         cm.save()
     }
+}
+
+/**
+ * Stands for getDNESet
+ */
+private fun Int.gds(s: String): Int {
+    return cm.getDNESet(s,this).result
+}
+
+/**
+ * Stands for getDNESet
+ */
+private fun String.gds(s: String): String {
+    return cm.getDNESet(s,this).result
 }
