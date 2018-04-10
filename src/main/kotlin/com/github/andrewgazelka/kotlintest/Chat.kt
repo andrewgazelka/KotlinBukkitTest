77ba41300b0dc0b920bd89421b3595eee24f5c42
Andrew Gazelka <andrew.gazelka@gmail.com>
2018-04-10 18:36:42 -0500

    add ability to drop hay

--- src/main/kotlin/com/github/andrewgazelka/kotlintest/Chat.kt
+++ src/main/kotlin/com/github/andrewgazelka/kotlintest/Chat.kt
@@ -4,6 +4,7 @@ import org.bukkit.ChatColor
 
 val red = ChatColor.RED
 val green = ChatColor.GREEN
+val yellow = ChatColor.YELLOW
 
 fun message(message: String) {
     KotlinTest.instance.server.broadcastMessage(message)
--- src/main/kotlin/com/github/andrewgazelka/kotlintest/KotlinTest.kt
+++ src/main/kotlin/com/github/andrewgazelka/kotlintest/KotlinTest.kt
@@ -1,5 +1,7 @@
 package com.github.andrewgazelka.kotlintest
 
+import org.bukkit.Location
+import org.bukkit.Material
 import org.bukkit.plugin.java.JavaPlugin
 
 class KotlinTest : JavaPlugin() {
@@ -12,9 +14,39 @@ class KotlinTest : JavaPlugin() {
         instance = this
         Config.reload()
         server.scheduler.scheduleSyncDelayedTask(this, { message("$green Enabled") }, Config.period_tick.toLong())
+        dropHay()
+    }
+
+    private fun dropHay() {
+        val world = server.worlds[0]
+        val location1 = Location(world, 0.0, 200.0, 0.0)
+        val location2 = Location(world, 5.0, 210.0, 5.0)
+        for (loc in location1..location2) {
+            world.spawnFallingBlock(loc, Material.HAY_BLOCK,0)
+            message("$yellow Dropping hay! (${loc.blockX},${loc.blockY},${loc.blockZ})")
+        }
     }
 
     override fun onDisable() {
         message("$red Disabled")
     }
+}
+
+private operator fun Location.rangeTo(location2: Location): List<Location> {
+    val list = ArrayList<Location>()
+    for (x in this.blockX to location2.blockX) {
+        for (y in this.blockY to location2.blockY) {
+            for (z in this.blockZ to location2.blockZ) {
+                list.add(Location(this.world, x.toDouble(), y.toDouble(), z.toDouble()))
+            }
+        }
+    }
+    return list
+}
+
+infix fun Int.to(to: Int): IntProgression {
+    if (this < to) {
+        return IntProgression.fromClosedRange(this, to, 1)
+    }
+    return IntProgression.fromClosedRange(this, to, -1)
 }
