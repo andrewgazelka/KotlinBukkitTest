32b98284fc5c3ee0c75ea8b1664fd6520d0003bf
Andrew Gazelka <andrew.gazelka@gmail.com>
2018-04-10 19:29:33 -0500

    make a tower

--- src/main/kotlin/com/github/andrewgazelka/kotlintest/Chat.kt
+++ src/main/kotlin/com/github/andrewgazelka/kotlintest/Chat.kt
@@ -1,11 +1,19 @@
 package com.github.andrewgazelka.kotlintest
 
 import org.bukkit.ChatColor
+import java.util.logging.Level
 
 val red = ChatColor.RED
 val green = ChatColor.GREEN
 val yellow = ChatColor.YELLOW
+val blue = ChatColor.BLUE
 
-fun message(message: String) {
-    KotlinTest.instance.server.broadcastMessage(message)
+fun message(message: String, public: Boolean = true) {
+    if(public)
+    {
+        KotlinTest.instance.server.broadcastMessage(message)
+    }
+    else{
+        KotlinTest.instance.logger.log(Level.INFO,message)
+    }
 }
--- src/main/kotlin/com/github/andrewgazelka/kotlintest/KotlinTest.kt
+++ src/main/kotlin/com/github/andrewgazelka/kotlintest/KotlinTest.kt
@@ -13,20 +13,23 @@ class KotlinTest : JavaPlugin() {
     override fun onEnable() {
         instance = this
         Config.reload()
-        drop(Material.TNT,doDamage = true)
-    }
-
-    private fun drop(material: Material, dataLow: Int = 0, dataHigh: Int = 1, doDamage: Boolean = false) {
 
         val world = server.worlds[0]
 
-        val location1 = Location(world, 0.0, 200.0, 0.0)
-        val location2 = Location(world, 5.0, 210.0, 5.0)
+        message("$blue 1st Layer",false)
+        drop(Material.STONE,Location(world, 0.0, 150.0, 0.0),Location(world, 5.0, 190.0, 5.0),doDamage = true)
+        message("$blue 2st Layer",false)
+        drop(Material.STONE,Location(world, 1.0, 190.0, 1.0),Location(world, 4.0, 210.0, 4.0),doDamage = true)
+        message("$blue 3rd Layer",false)
+        drop(Material.STONE,Location(world, 2.0, 210.0, 2.0),Location(world, 3.0, 230.0, 3.0),doDamage = true)
+    }
+
+    private fun drop(material: Material, location1: Location, location2: Location, dataLow: Int = 0, dataHigh: Int = 1, doDamage: Boolean = false) {
 
         for (loc in location1..location2) {
-            val fallingBlock = world.spawnFallingBlock(loc, material, (dataLow randTo dataHigh).toByte())
+            val fallingBlock = loc.world.spawnFallingBlock(loc, material, (dataLow randTo dataHigh).toByte())
             fallingBlock.setHurtEntities(doDamage)
-            message("$yellow Dropping ${material.name}! (${loc.blockX},${loc.blockY},${loc.blockZ})")
+            message("$yellow Dropping ${material.name}! (${loc.blockX},${loc.blockY},${loc.blockZ})",false)
         }
     }
 
